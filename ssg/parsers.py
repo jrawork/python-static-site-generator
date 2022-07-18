bb33662afc9cd69de3673d1deb17cde243d7bd80
a685065 <joseph.ahrendsen@fmr.com>
2022-07-18 16:13:14 -0400

    4th attempt module 3

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -7,10 +7,7 @@ class Parser:
     extensions: List[str] = []
 
     def valid_extension(self, extension):
-        if extension in self.extensions:
-            return True
-        else:
-            return False
+        return extension in self.extensions
 
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
