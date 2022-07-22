728f5178d5b0f2a3fe99ad5119b796427bbc5189
a685065 <joseph.ahrendsen@fmr.com>
2022-07-22 09:40:01 -0400

    module 4 attempt 2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -40,7 +40,20 @@ class MarkdownParser(Parser):
     extensions = [".md", ".markdown"]
     
     def parse(self, path: Path, source: Path, dest: Path):
-        Content.load(self.read(), path)
-        html = markdown(Content.body)
+        content = Content.load(self.read(path))
+        html = markdown(content.body)
         self.write(path, dest, html)
-        sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n").format(path.name, Content)  # This string changes color of string in terminal 
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, Content) # This string changes color of string in terminal 
+        )  
+
+class RestructuredTextParser(Parser):
+    extensions = [".rst"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        content = Content.load(self.read(path))
+        html = publish_parts(content.body, writer_name="html5")
+        self.write(path, dest, html["html_body"])
+        sys.stdout.write(
+            "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
+        )
